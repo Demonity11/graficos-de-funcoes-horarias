0756a85afa8b0b102852617ec7935a7e224e0cf0
Demonity11 <104987423+Demonity11@users.noreply.github.com>
2025-11-05 06:14:51 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request, session, url_for, redirect
-from os import environ, getenv
+from os import getenv
 from dotenv import load_dotenv
 from math import ceil
-
-load_dotenv()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = getenv("SECRET_KEY")
